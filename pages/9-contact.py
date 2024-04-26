7b1030336d4fc1a2f312dd01292c045788b6b4c4
Frank Inyiama <frankinyiama@gmail.com>
2024-04-26 18:22:46 -0500

    Merging changes

--- pages/9-contact.py
+++ pages/9-contact.py
@@ -10,13 +10,10 @@ left_co, cent_co, last_co = st.columns(3)
 with cent_co:
     st.image('fisk_logo_transparent.jpeg')
 
-display_sponsored_research('Contact')
+display_sponsored_research('Contacts')
 
-
-col1, col2, col3 = st.columns([7.5, 10, 5]) 
-col2.title("Contacts")
 col1, col2, col3 = st.columns([10, 2, 10]) 
-col1.image("/home/mayaajike/sponsored-research-page/sajid-hussain-headshot.jpeg", width=255)
+col1.image("sajid-hussain-headshot.jpeg", width=255)
 col1.write(
 '''**Dr. Sajid Hussain, PhD** \n
 **Director, Office of Sponsored Research and Programs** \n
@@ -25,7 +22,7 @@ Office: Cravath Hall, Room 110B \n
 Email: shussain@fisk.edu''')
 
 
-col3.image("/home/mayaajike/sponsored-research-page/Arnold-Burger-headshot-fisk-research-page.jpg", width=180)
+col3.image("Arnold-Burger-headshot-fisk-research-page.jpg", width=180)
 col3.write('''
 **Arnold Burger, PhD** \n
 **Sr. Vice Provost for Faculty Initiatives** \n
